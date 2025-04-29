04c3b26dd753315eb2446d8c77bca59d49a1e794
Andressa Cinelli <acdias.96@outlook.com>
2025-04-29 15:26:22 +0000

    Nível aventureiro concluido

--- xadrexnovato.c
+++ xadrexnovato.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 int main(){
 
     int movimentobispo = 5;
     int movimentorainha = 8;
     int movimentotorre = 5;
-    
-    int i;
+    int cavalo_esquerda = 1;
+    int cavalo_baixo = 2;
+    int i, j, k, l;
 
     printf("--- Jogo Xadrez ---\n");
     
@@ -14,7 +16,8 @@ int main(){
     {
         printf("Cima Direita\n");
     }
-    
+        printf ("\n");
+
     printf("*** Movimento Rainha: ***\n");     // movimento da rainha usando o loop while.
     i = 0;
     while (i < movimentorainha)
@@ -23,12 +26,38 @@ int main(){
         i++;
     }
     
+        printf("\n");
+
     printf ("*** Movimento Torre: ***\n");   // movimento da torre usando loop do-while.
     i = 0;
     do{
         printf("Direita \n");
         i++;
     } while (i < movimentotorre);
+
+        printf("\n");
+
+    printf ("*** Movimento Cavalo ***\n");
+    for (i = 0; i <cavalo_baixo; i++)
+    {
+        printf ("Baixo\n");
+        
+        for (j = 0; j < cavalo_baixo; j++){
+            
+        }
+    } 
+
+    
+    while (k < cavalo_esquerda){
+        printf ("Esquerda\n");
+        k++;
+    }
+   
+    while (l < cavalo_esquerda){
+        l++;
+    }
+
+
     
     return 0;
-} 
+}
